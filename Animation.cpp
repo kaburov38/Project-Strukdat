025a886d13fa71a92e1cfabe213eff540a16d2a5
Excel90 <57035606+Yehezkiel@users.noreply.github.com>
2020-11-30 10:17:09 +0700

    asset+animation

---
+++ Animation.cpp
@@ -0,0 +1,32 @@
+#include "Animation.h"
+
+Animation::Animation(sf::Texture* texture, sf::Vector2u ImageCount, float Switchtime)
+{
+	this->ImageCount = ImageCount;
+	this->SwitchTime = Switchtime;
+	totalTime = 0.0f;
+	CurrentImage.x = 0;
+	uvRect.width = texture->getSize().x / float(ImageCount.x);
+	uvRect.height = texture->getSize().y / float(ImageCount.y);
+}
+Animation::~Animation(){
+
+}
+
+void Animation::Update(int row, float deltaTime)
+{
+	CurrentImage.y = row;
+	totalTime += deltaTime;
+	if (totalTime >= SwitchTime)
+	{
+		totalTime -= SwitchTime;
+		CurrentImage.x++;
+
+		if (CurrentImage.x >= ImageCount.x)
+		{
+			CurrentImage.x = 0;
+		}
+	}
+	uvRect.left = CurrentImage.x * uvRect.width;
+	uvRect.top = CurrentImage.y * uvRect.height;
+}
---
+++ Animation.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Animation
+{
+public:
+	Animation(sf::Texture* texture, sf::Vector2u ImageCount, float Switchtime);
+	~Animation();
+	void Update(int row, float deltaTime);
+
+public:
+	sf::IntRect uvRect;
+
+private:
+	sf::Vector2u ImageCount;
+	sf::Vector2u CurrentImage;
+	float totalTime;
+	float SwitchTime;
+};
+
